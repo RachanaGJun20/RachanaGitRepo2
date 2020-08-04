eca9770db4bf9d289d34dd80fe085d63cc125f9a
Rachana Ghayal <rach.g14@gmail.com>
2020-08-04 18:45:21 +0530

    SortingUsingSet

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -1,9 +1,25 @@
 package p1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello Rachana");
+		List<Integer> list = new ArrayList<>();
+		
+		list.add(5);
+		list.add(3);
+		list.add(6);
+		list.add(4);
+		
+		System.out.println(list);
+		
+		Set<Integer> set = new HashSet<>(list);
+		
+		System.out.println(set);
 	}
 
 }
